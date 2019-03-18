97268ced884cb246bfc146884b611b90fde6765f
Keita Akutsu <kakts.git@gmail.com>
2019-03-18 21:50:11 +0900

    Fix repl.go

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"github.com/kakts/monkey/token"
 	"github.com/kakts/monkey/lexer"
+	"github.com/kakts/monkey/parser"
 )
 
 // replの先頭文字
@@ -24,10 +24,24 @@ func Start(in io.Reader, out io.Writer) {
 		// 入力行の内容取得
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		// EOFがくるまで入力内容を表示
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
