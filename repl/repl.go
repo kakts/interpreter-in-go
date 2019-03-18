e513c19b87fea05119c2ace24e86ac269c7ad1b7
Keita Akutsu <kakts.git@gmail.com>
2019-03-19 00:06:22 +0900

    Add Evaluator into repl.go

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"github.com/kakts/monkey/lexer"
 	"github.com/kakts/monkey/parser"
+	"github.com/kakts/monkey/evaluator"
 )
 
 // replの先頭文字
@@ -33,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
