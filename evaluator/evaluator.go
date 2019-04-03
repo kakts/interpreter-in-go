95ee5941a60b648759123dd7134423752d104559
Keita Akutsu <kakts.git@gmail.com>
2019-04-04 00:56:30 +0900

    Add String concatenation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -197,6 +197,9 @@ func evalInfixExpression (
 		return nativeBoolToBooleanObject(left != right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
+	
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
@@ -341,4 +344,15 @@ func unwrapReturnValue(obj object.Object) object.Object {
 	}
 
 	return obj
+}
+
+// 文字列の連結処理
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+  if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
 }
